4cb382487994d074247cc14c0d6c9ab10f4dc624
ltizzle12 <blah@blah.com>
2024-10-13 14:21:20 -0500

    Project 3 work

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template, request  # NOT the same as requests 
+from flask import Flask, render_template, request  # NOT the same as requests
+# from github_api import get_github_user
+#TODO - Add APIs here to import
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # home page - this is returning "index.html"
 def homepage():
     return render_template('index.html')
+
+@app.route('/get_artist') # don't forget the /
+def get_user_info():
+    # get user info from an API (figure out which one)
+    print('form data is', request.args)
+    # this will read the artist that is being searched for
+    artistname = request.args.get('artist_name')
+    #TODO - call the funciton from the API's
+    # this will call the function from github_api.py in the video
+    #user_info, error message = get_github_user(username) from video or]
+    # if error_message:
+    #     return render_template('error.html,', error=error_message)
+    # else:
+    #     return render_template('github.html', user_info=user_info)
+    artist_info, error message = get_artist_name(artist_name)
+    if error_message:
+        return render_template('error.html,', error=error_message)
+    else:
+        return render_template('github.html', artist_info=artist_info) #TODO change github.html
+
+    #return render_template('API.html', artist_info=artist_info)
+    # using parameters, this will go to 'API_html' above and search the artist's name
+    return render_template('API.html', music_name=artistname)
+    # return
+    # TODO - this will call the user_info
 
 
 if __name__ == '__main__':
--- github_api.py
+++ github_api.py
@@ -1,1 +1,27 @@
-# TODO make requests to github API
+# TODO make requests to Spotify, Bandsintown, Youtube API
+
+# The other students will have the API Imports
+
+import requests
+import logging
+
+def get_artist_name(artist_name):
+
+    #error handling
+
+    try:
+        response = requests.get() # TODO - insert API for seaching artists name
+        if response.status_code == 404: #not found
+            return None, f'Music Artist {artist_name} not found' # username will be artist_name
+        response.raise_for_status()
+        response_json = response.json()
+        artist_info = extract_artist_info(response_json)
+        return artist_info, None
+    except Exception as e:
+        logging.exception(e)
+        return None, 'Error connecting to API'
+
+def extract_artist_info(json_response): # TODO inset keys that the API will call
+    return {
+        'login': json_response.get('login'),
+    }
