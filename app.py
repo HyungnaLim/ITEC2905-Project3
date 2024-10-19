171b0da1e557b44268d33f348d70eef9931fbe0d
ltizzle12 <blah@blah.com>
2024-10-18 20:31:02 -0500

    added try and except
    # Conflicts:
    #	app.py

--- app.py
+++ app.py
@@ -16,20 +16,29 @@ def get_artist_info():
 
     # this will read the artist that is being searched for
     artist_name = request.args.get('artist_name')
+    try: # Attempt to get artist info from Spotify
+        artist_info = spotify.main(artist_name)
+        if artist_info: # This will fetch the events and music video
+            events_info = events.main(artist_name)
+            music_video = video.main(f'{artist_info.artist} {artist_info.tracks[0]['title']}')
+            return render_template('search_result.html',
+                                   artist_name=artist_info.artist,
+                                   artist_img=artist_info.image_url,
+                                   artist_genres=artist_info.genres_str(),
+                                   artist_tracks=artist_info.tracks,
+                                   music_video=music_video,
+                                   events_info=events_info),
+        else:
+            return render_template('error.html', error_message=f"Artist '{artist_name}' not found on Spotify.")
 
-    artist_info = spotify.main(artist_name)
-    events_info = events.main(artist_name)
-    music_video = video.main(f'{artist_info.artist} {artist_info.tracks[0]['title']}')
+    except Exception as e:
+        # Handle unexpected errors
+        print(f"An error occurred: {e} Please return to the main page")
+        return render_template('error.html', error_message="An unexpected error occurred.")
 
-    # TODO add exception handling
-    # TODO
-    return render_template('search_result.html',
-                           artist_name=artist_info.artist,
-                           artist_img=artist_info.image_url,
-                           artist_genres=artist_info.genres_str(),
-                           artist_tracks=artist_info.tracks_str(),
-                           music_video=music_video,
-                           events_info=events_info)
+    # return a boolean that is no artist found
+    # if spotify returns false, return render template error that spotify gave it
+
 
 
 if __name__ == '__main__':
