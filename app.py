1db74b918d6b3e59541f0b0014c7a13957a3b895
rwalk651 <user@example.com>
2024-10-19 20:33:43 -0400

    messing with db

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request  # NOT the same as requests
 import apis.spotify_api as spotify
 import apis.youtube_api as video
 import apis.ticketmaster_api as events
+import db.search_results_db as db
 
 app = Flask(__name__)
 
@@ -18,11 +19,15 @@ def get_artist_info():
     artist_name = request.args.get('artist_name')
 
     artist_info = spotify.main(artist_name)
+    db.store_artist_info(artist_info)
     events_info = events.main(artist_name)
     music_video = video.main(f'{artist_info.artist} {artist_info.tracks[0]['title']}')
+    # send_to_db =
+    # db.store_data([
+    #     artist_info, events_info, music_video
+    # ])
 
     # TODO add exception handling
-    # TODO
     return render_template('search_result.html',
                            artist_name=artist_info.artist,
                            artist_img=artist_info.image_url,
--- db/search_results_db.py
+++ db/search_results_db.py
@@ -1,5 +1,5 @@
 from peewee import *
-from datetime import date
+from datetime import date, datetime
 import logging
 
 logger = logging.getLogger('peewee')
@@ -13,63 +13,40 @@ class BaseModel(Model):
 
 class Artist(BaseModel):
     name = CharField(unique=True)
-    last_updated = DateField()
+    img_url = CharField()
+    date_created = DateField()
 
     def __str__(self):
-        return f'{self.name}, {self.last_updated}'
+        return f'{self.name}, {self.img_url}, {self.date_created}'
 
 class Album(BaseModel):
     name = CharField(unique=True)
-    release_date = CharField()
-    last_updated = DateField()
+    date_created = DateField()
     artist = ForeignKeyField(Artist, backref='albums')
 
     def __str__(self):
-        return f'{self.name}, {self.release_date}, {self.last_updated}'
+        return f'{self.name}, {self.date_created}'
 
 class Track(BaseModel):
     title = CharField(unique=True)
+    album = CharField()
+    release_date = CharField()
     spotify_url = CharField()
-    last_updated = DateField()
+    date_created = DateField()
     artist = ForeignKeyField(Artist, backref='tracks')
-    album = ForeignKeyField(Album, backref='tracks')
 
     def __str__(self):
-        return f'{self.title}, {self.spotify_url}, {self.last_updated}'
+        return f'{self.title}, {self.album}, {self.release_date}, {self.spotify_url}, {self.date_created}'
 
-db.connect()
-db.create_tables([Artist, Album, Track])
+class Genre(BaseModel):
+    genre = ForeignKeyField(Track, unique=True)
+
+    def __str__(self):
+        return f'{self.genre}'
 
 Artist.delete().execute()
 Album.delete().execute()
 Track.delete().execute()
-
-def sample_data():
-    radiohead = Artist(name='Radiohead', last_updated=date.today())
-    radiohead.save()
-    print(radiohead.name)
-
-    album = Album(artist=radiohead.name, name='Pablo Honey', release_date='02/22/93', last_updated=date.today())
-    album.save()
-
-    track = Track(artist=radiohead.name, title='Creep', album='Pablo Honey', spotify_url='url', last_updated=date.today())
-    track.save()
-
-    oasis = Artist(name='Oasis', last_updated=date.today())
-    oasis.save()
-
-    oasis_album = Album(artist=oasis.name, name='(Whats The Story) Morning Glory?', release_date='1995',
-                        last_updated=date.today())
-    oasis_album.save()
-
-    oasis_album = Album(artist=oasis.name, name='Heathen Chemistry', release_date='2002-07-01',
-                        last_updated=date.today())
-    oasis_album.save()
-
-    oasis_track = Track(artist=oasis.name, title='Wonderwall', album=oasis_album.name, spotify_url='url2', last_updated=date.today())
-    oasis_track.save()
-
-    print('created!')
 
 def main():
     menu_text = """
@@ -99,10 +76,38 @@ def main():
         #     edit_existing_record()
         # elif choice == '5':
         #     delete_record()
-        elif choice == '6':
+        elif choice == '9':
             break
         else:
             print('Not a valid selection, please try again')
+
+def store_artist_info(spotify_data):
+    print(spotify_data)
+    artist, created = Artist.get_or_create(
+        name=spotify_data.artist,
+        defaults={'img_url': spotify_data.image_url, 'date_created': datetime.today()}
+    )
+    if created:
+        print(f'{artist} saved!. Storing tracks...')
+        store_track_info(spotify_data)
+    else:
+        print(f'{artist} already in database. Storing tracks...')
+        store_track_info(spotify_data)
+
+def store_track_info(spotify_data):
+    for title in spotify_data.tracks['title']:
+        track, created = Track.get_or_create(
+            name=title,
+            defaults={'album': spotify_data.tracks['album'],
+                      'release_date': spotify_data.tracks['release date'],
+                      'spotify_url': spotify_data.tracks['spotify url'],
+                      'date_created': datetime.today(),
+                      'artist': spotify_data.artist}
+        )
+        if created:
+            print(f'{track} stored!')
+        else:
+            print(f'{track} already in database.')
 
 def display_all_artists():
     artists = Artist.select()
@@ -118,9 +123,37 @@ def display_all_albums():
 def display_all_tracks():
     tracks = Track.select()
     for track in tracks:
-        # print(f'{track.artist_id}: {track.album_id}', track)
-        print(f'{track.title} - {track.artist_id}, {track.album_id}, {track.spotify_url}, {track.last_updated}')
+        print(f'{track.title} - {track.artist_id}, {track.album}, {track.release_date}, '
+              f'{track.spotify_url}, {track.date_created}')
+
+def sample_data():
+    radiohead = Artist(name='Radiohead', img_url='url', date_created=date.today())
+    radiohead.save()
+
+    album = Album(artist=radiohead.name, name='Pablo Honey', date_created=date.today())
+    album.save()
+
+    track = Track(artist=radiohead.name, title='Creep', release_date='02/22/93', album='Pablo Honey',
+                  spotify_url='url', date_created=date.today())
+    track.save()
+
+    oasis = Artist(name='Oasis', img_url='url', date_created=date.today())
+    oasis.save()
+
+    oasis_album = Album(artist=oasis.name, name='(Whats The Story) Morning Glory?', date_created=date.today())
+    oasis_album.save()
+
+    oasis_album = Album(artist=oasis.name, name='Heathen Chemistry', date_created=date.today())
+    oasis_album.save()
+
+    oasis_track = Track(artist=oasis.name, title='Wonderwall', release_date='1995',
+                        album=oasis_album.name, spotify_url='url2', date_created=date.today())
+    oasis_track.save()
+
+    print('created!')
 
 
 if __name__ == '__main__':
+    # db.connect()
+    db.create_tables([Artist, Album, Track])
     main()
