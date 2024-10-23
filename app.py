3264aa00de562fd58c74261849261329a58f54db
Hyungna <hyungnalim@gmail.com>
2024-10-23 18:54:35 -0500

    added detail to error handling

--- app.py
+++ app.py
@@ -18,9 +18,12 @@ def get_artist_info():
     artist_name = request.args.get('artist_name')
 
     artist_info = spotify.main(artist_name)
+
     if artist_info is None:
         return render_template('error.html', error='Spotify API error: cannot get artist info')
     else:
+        if artist_info.artist is not None:
+            artist_name = artist_info.artist
         events_info = events.main(artist_name)
         if artist_info.tracks is not None:
             music_video = video.main(f'{artist_info.artist} {artist_info.tracks[0]['title']}')
@@ -34,7 +37,7 @@ def get_artist_info():
             artist_track_three = None
 
         return render_template('search_result.html',
-                               artist_name=artist_info.artist,
+                               artist_name=artist_name,
                                artist_img=artist_info.image_url,
                                artist_genres=artist_info.genres_str(),
                                artist_track_one=artist_track_one,
