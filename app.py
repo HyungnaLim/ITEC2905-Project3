bda4a12f2f48bfb45a05c426be13873006a12a41
rwalk651 <user@example.com>
2024-10-23 07:30:12 -0400

    still trying to figure out how to save search

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request  # NOT the same as requests
+from flask import Flask, render_template, flash, redirect, session, request, url_for  # NOT the same as requests
+from peewee import *
 import apis.spotify_api as spotify
 import apis.youtube_api as video
 import apis.ticketmaster_api as events
 
 app = Flask(__name__)
+db = SqliteDatabase('temp.db')
+
+class TempSearch(Model):
+    
 
 @app.route('/') # home page
 def homepage():
@@ -21,8 +26,6 @@ def get_artist_info():
     events_info = events.main(artist_name)
     music_video = video.main(f'{artist_info.artist} {artist_info.tracks[0]['title']}')
 
-    # TODO add exception handling
-    # TODO
     return render_template('search_result.html',
                            artist_name=artist_info.artist,
                            artist_img=artist_info.image_url,
@@ -31,6 +34,21 @@ def get_artist_info():
                            music_video=music_video,
                            events_info=events_info)
 
+
+
+@app.route('/save_search', methods=['POST'])
+def save_artist():
+    artist = session.get('user_search')
+    print(artist)
+    store_search_data(artist)
+    flash('Artist saved!')
+    # replace index with bookmarks.html
+    return redirect(url_for('index'))
+
+def store_search_data(artist):
+    # send to search_results_db
+    # some_save_function_to_db(artist)
+    print(f'Saved {artist}')
 
 if __name__ == '__main__':
     app.run()
