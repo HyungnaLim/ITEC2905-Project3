4f708234200026b5f88fa501ae1f8d3d84aef9bd
Hyungna <hyungnalim@gmail.com>
2024-10-23 19:13:41 -0500

    added try-except block for unexpected exception in app.py

--- app.py
+++ app.py
@@ -11,43 +11,49 @@ def homepage():
 
 @app.route('/get_artist')
 def get_artist_info():
-    # get user info from an API (figure out which one)
-    print('form data is', request.args)
 
-    # this will read the artist that is being searched for
-    artist_name = request.args.get('artist_name')
+    try:
+        # get user info from an API (figure out which one)
+        print('form data is', request.args)
 
-    artist_info = spotify.main(artist_name)
+        # this will read the artist that is being searched for
+        artist_name = request.args.get('artist_name')
 
-    if artist_info is None:
-        return render_template('error.html', error='Spotify API error: cannot get artist info')
-    else:
-        if artist_info.artist is not None:
-            artist_name = artist_info.artist
-        events_info = events.main(artist_name)
-        if artist_info.tracks is not None:
-            music_video = video.main(f'{artist_info.artist} {artist_info.tracks[0]['title']}')
-            music_video_title = music_video['video_title']
-            artist_track_one = artist_info.tracks[0]
-            artist_track_two = artist_info.tracks[1]
-            artist_track_three = artist_info.tracks[2]
+        artist_info = spotify.main(artist_name)
+
+        if artist_info is None:
+            return render_template('error.html', error='Spotify API error: cannot get artist info')
         else:
-            music_video = None
-            music_video_title = 'Music Video'
-            artist_track_one = None
-            artist_track_two = None
-            artist_track_three = None
+            if artist_info.artist is not None:
+                artist_name = artist_info.artist
+            events_info = events.main(artist_name)
+            if artist_info.tracks is not None:
+                music_video = video.main(f'{artist_info.artist} {artist_info.tracks[0]['title']}')
+                music_video_title = music_video['video_title']
+                artist_track_one = artist_info.tracks[0]
+                artist_track_two = artist_info.tracks[1]
+                artist_track_three = artist_info.tracks[2]
+            else:
+                music_video = None
+                music_video_title = 'Music Video'
+                artist_track_one = None
+                artist_track_two = None
+                artist_track_three = None
 
-        return render_template('search_result.html',
-                               artist_name=artist_name,
-                               artist_img=artist_info.image_url,
-                               artist_genres=artist_info.genres_str(),
-                               artist_track_one=artist_track_one,
-                               artist_track_two=artist_track_two,
-                               artist_track_three=artist_track_three,
-                               music_video=music_video,
-                               music_video_title=music_video_title,
-                               events_info=events_info)
+            return render_template('search_result.html',
+                                   artist_name=artist_name,
+                                   artist_img=artist_info.image_url,
+                                   artist_genres=artist_info.genres_str(),
+                                   artist_track_one=artist_track_one,
+                                   artist_track_two=artist_track_two,
+                                   artist_track_three=artist_track_three,
+                                   music_video=music_video,
+                                   music_video_title=music_video_title,
+                                   events_info=events_info)
+    except Exception as e:
+        # Handle unexpected errors
+        print(f"An error occurred: {e} Please return to the main page")
+        return render_template('error.html', error="An unexpected error occurred.")
 
 
 if __name__ == '__main__':
