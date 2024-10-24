2c8dc52538419c9eb602905a47e249c7b1240872
rwalk651 <user@example.com>
2024-10-24 18:36:48 -0400

    return data from youtube api in correct format

--- apis/youtube_api.py
+++ apis/youtube_api.py
@@ -14,6 +14,7 @@ service_name = 'youtube'
 service_version = 'v3'
 # api_key = os.environ.get('DEVELOPER_KEY')   # insert api key into DEVELOPER_KEY env variable
 
+
 def main(search_term):
     video_title = 'items(snippet/title)'
     video_id = 'items(id/videoId)'
@@ -21,36 +22,25 @@ def main(search_term):
 
     try:
         with (build(service_name, service_version, developerKey='AIzaSyDwqLyFMv40cYjRW8jUEQyBgD-nvxR_PwY')
-              as youtube_build):
-            youtube_query = youtube_build.search().list(
-            part='snippet',
-            maxResults=1,
-            q=f'{search_term} music video',
-            type='video',
-            videoCategoryId=10,
-            fields=f'{video_title},{video_id},{video_thumbnail}' # returns only specific data
-        )
+              as request):
+            response = request.search().list(
+                    part='snippet',
+                    maxResults=1,
+                    q=f'{search_term} music video',
+                    type='video',
+                    videoCategoryId=10,
+                    fields=f'{video_title},{video_id},{video_thumbnail}'
+                ).execute()
 
-        response, error_message = request_youtube_video(youtube_query)
-        chosen_video, error_message = api_data_extraction(response)
+        youtube_video, error_message = response_data_extraction(response)
         if error_message:
             return None, error_message
-        return chosen_video, None
-
-    except Exception as e:
-        print(f'{e}')
-        return None, e
-
-
-def request_youtube_video(request_video):
-    try:
-        youtube_video = request_video.execute()
         return youtube_video, None
 
     except (UnknownApiNameOrVersion, HttpError, Exception) as e:
         match e:
             case UnknownApiNameOrVersion():
-                err_msg = f'Error unknown YouTube API name or version: {e}, supported APIs: {api_name_version_index}'
+                err_msg = f'Error unknown YouTube API name or version: {e}. Supported APIs: {api_name_version_index}'
                 return None, err_msg
             case HttpError():
                 err_msg = f'Error YouTube response status code: {e.status_code}, reason: {e.error_details}'
@@ -60,7 +50,7 @@ def request_youtube_video(request_video):
                 return None, err_msg
 
 
-def api_data_extraction(api_response_data):
+def response_data_extraction(api_response_data):
     try:
         for data in api_response_data.get('items', []):
             video_title = html.unescape(data['snippet']['title'])
--- app.py
+++ app.py
@@ -11,35 +11,34 @@ def homepage():
 
 @app.route('/get_artist')
 def get_artist_info():
-    try:
-        # get user info from an API (figure out which one)
-        print('form data is', request.args)
+    # get user info from an API (figure out which one)
+    print('form data is', request.args)
 
-        # this will read the artist that is being searched for
-        artist_name = request.args.get('user_search_query')
+    # this will read the artist that is being searched for
+    artist_name = request.args.get('user_search_query')
 
-        # api return format: (api_data, None)
-        artist_info, error_message = spotify.main(artist_name)
-        events_info, error_message = events.main(artist_info.artist)
-        music_video, error_message = video.main(f'{artist_info.artist} {artist_info.tracks[0]['title']}')
+    artist_info = spotify.main(artist_name)
+    events_info = events.main(artist_info.artist)
 
-        # first if-clause will trigger if api returns (None, error_message)
-        if error_message:
-            return render_template('error.html', error=error_message)
-        else:
-            return render_template('search_result.html',
-                                   artist_name=artist_info.artist,
-                                   artist_img=artist_info.image_url,
-                                   artist_genres=artist_info.genres_str(),
-                                   artist_track_one=artist_info.tracks[0],
-                                   artist_track_two=artist_info.tracks[1],
-                                   artist_track_three=artist_info.tracks[2],
-                                   music_video=music_video,
-                                   events_info=events_info)
+    # artist_info, error_message = spotify.main(artist_name)
+    # events_info, error_message = events.main(artist_info.artist)
 
-    except Exception as e:
-        print(f'{e}')
-        # return render_template('error.html', error=e)
+    # api return format: (api_data, None)
+    music_video, error_message = video.main(f'{artist_info.artist} {artist_info.tracks[0]['title']}')
+
+    # first if-clause will trigger if api returns (None, error_message)
+    if error_message:
+        return render_template('error.html', error=error_message)
+
+    return render_template('search_result.html',
+                           artist_name=artist_info.artist,
+                           artist_img=artist_info.image_url,
+                           artist_genres=artist_info.genres_str(),
+                           artist_track_one=artist_info.tracks[0],
+                           artist_track_two=artist_info.tracks[1],
+                           artist_track_three=artist_info.tracks[2],
+                           music_video=music_video,
+                           events_info=events_info)
 
 
 if __name__ == '__main__':
