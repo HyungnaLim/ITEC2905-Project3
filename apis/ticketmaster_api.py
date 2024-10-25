914481a1109bb390f6661393c64de553854828ef
SAPHIR KITOKO <saphir.kitoko@go.minneapolis.edu>
2024-10-25 08:18:13 -0500

    changed ticketmaster api to return more events

--- apis/ticketmaster_api.py
+++ apis/ticketmaster_api.py
@@ -3,56 +3,76 @@ import os
 import urllib.parse
 
 class TicketmasterEvent:
-    def __init__(self, name, date, venue):
+    def __init__(self, name, date, venue, artist):
         self.name = name
         self.date = date
         self.venue = venue
+        self.artist = artist
 
     def __str__(self):
-        return f"Event: {self.name}, Date: {self.date}, Venue: {self.venue}"
+        return f"Event: {self.name} by {self.artist}, Date: {self.date}, Venue: {self.venue}"
 
 class TicketmasterAPI:
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def search_events(self, search_term):
+    def search_events(self, search_term, size=20):
         encoded_search_term = urllib.parse.quote(search_term)
-        events_url = f'https://app.ticketmaster.com/discovery/v2/events.json?apikey={self.api_key}&keyword={encoded_search_term}&size=1'
+        page = 0
+        all_events = []
 
-        try:
-            response = requests.get(events_url)
-            response.raise_for_status()
-            events_data = response.json()
+        while True:
+            events_url = (f'https://app.ticketmaster.com/discovery/v2/events.json?apikey={self.api_key}&keyword={encoded_search_term}'
+                           f'&size={size}&page={page}')
 
-            if '_embedded' in events_data and 'events' in events_data['_embedded']:
-                events = []
-                for event in events_data['_embedded']['events']:
-                    name = event['name']
-                    date = event['dates']['start']['localDate']
-                    venue = event['_embedded']['venues'][0]['name']
-                    events.append(TicketmasterEvent(name, date, venue))
-                return events
-            else:
-                return {'error': 'No events found'}
+            try:
+                response = requests.get(events_url)
+                response.raise_for_status()
+                events_data = response.json()
 
-        except requests.exceptions.HTTPError as http_err:
-            print(f'HTTP error occurred: {http_err}')
-            return {'error': str(http_err)}
-        except Exception as err:
-            print(f'An error occurred: {err}')
-            return {'error': str(err)}
+                if '_embedded' in events_data and 'events' in events_data['_embedded']:
+                    for event in events_data['_embedded']['events']:
+                        name = event['name']
+                        date = event['dates']['start'].get('localDate', 'N/A')
+                        venue = event['_embedded']['venues'][0]['name']
+
+                        # Attempt to extract artist name if available
+                        artist = ''
+                        if '_embedded' in event and 'attractions' in event['_embedded']:
+                            artist = ', '.join(attraction['name'] for attraction in event['_embedded']['attractions'])
+
+                        all_events.append(TicketmasterEvent(name, date, venue, artist))
+
+                    # Check for pagination
+                    if 'page' in events_data and events_data['page'].get('hasNext', False):
+                        page += 1
+                    else:
+                        break
+                else:
+                    print("No events found.")
+                    break
+
+            except requests.exceptions.HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')
+                return {'error': str(http_err)}
+            except Exception as err:
+                print(f'An error occurred: {err}')
+                return {'error': str(err)}
+
+        return all_events
 
 def main(search_term):
     TICKETMASTER_API_KEY = os.getenv('TICKETMASTER_API_KEY', 'GSODZqdDGBo7UGEzXzglQDxeo3fhhdW5')
     ticketmaster = TicketmasterAPI(TICKETMASTER_API_KEY)
 
-    # search_term = input("Enter artist name to search for events: ")
-    events = ticketmaster.search_events(search_term)
+    events = ticketmaster.search_events(search_term, size=50)
 
-    if isinstance(events, list):
+    if isinstance(events, list) and events:
         for event in events:
-            # print(event)
-            return event
+            print(event)
     else:
-        # print(events)
-        return events
+        print(events)
+
+if __name__ == "__main__":
+    artist_name = input("Enter artist name to search for events: ")
+    main(artist_name)
